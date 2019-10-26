da75bf702b17f7eadc7ea61bcc2109d9162f17af
Alexei-Str <lesha9798@gmail.com>
2019-10-26 23:13:43 +0300

    hw4 completed

---
+++ app/src/main/java/com/example/hw4_intent/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.hw4_intent
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(){
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Hi")
+                type = "text/plain"
+            }
+            startActivityForResult(sendIntent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "Shared", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Is Failed message", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hw4_intent/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.hw4_intent
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val value = intent.getStringExtra(Intent.EXTRA_TEXT)
+        textView.text = ""+value
+    }
+}
